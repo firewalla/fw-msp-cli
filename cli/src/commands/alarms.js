872b57491b60e04d736ca2658160a2dcd875825d
hhiro <hirokazu@firewalla.com>
2026-03-30 11:42:40 -0700

    Update alarms.js

--- cli/src/commands/alarms.js
+++ cli/src/commands/alarms.js
@@ -2,8 +2,8 @@ const { getClient, resolveBoxGid, loadJson } = require('../api/client');
 
 const Alarms = {
   list: async (options) => {
-    const gid = await resolveBoxGid(options.box, options.debug);
-    const client = getClient(options.debug);
+    const gid = await resolveBoxGid(options.box, options);
+    const client = getClient(options);
     
     let apiParams = { gid };
     if (options.params) {
@@ -14,13 +14,13 @@ const Alarms = {
       const { data } = await client.get('/alarms', { params: apiParams });
       console.log(JSON.stringify(data, null, 2));
     } catch (err) {
-      console.error(JSON.stringify({ error: "API Error", details: err.response?.data || err.message }));
+      console.error(JSON.stringify({ error: "Fetch failed", details: err.response?.data || err.message }));
     }
   },
 
   update: async (id, options) => {
-    const gid = await resolveBoxGid(options.box, options.debug);
-    const client = getClient(options.debug);
+    const gid = await resolveBoxGid(options.box, options);
+    const client = getClient(options);
     const body = loadJson(options.json);
 
     try {
